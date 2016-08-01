cd054d31f25ee002feb775a7fd60252701fd7abf
Nikolay <kolysh23@yandex.ru>
2016-08-01 03:10:10 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Николай
+ * Date: 01.08.2016
+ * Time: 2:57
+ */
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once __DIR__ . "/config.php";
+
+if ($_SERVER['REQUEST_URI'] == '/') {
+    $page = "index";
+    $module = 'index';
+} else {
+
+    $URI_Path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+
+}
+
+
